d18ac5a405f9ca2e28da1b2367e2899830cd46d7
Danillo Soares <danillohss@gmail.com>
2023-09-12 15:00:08 -0300

    nuxtjs - conhecendo o framework

---
+++ pages/servicos.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <div class="bg-gray-100">
+        <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
+          <NuxtLink :to="{ name: 'a-empresa' }">Empresa</NuxtLink>
+          <NuxtLink to="/servicos">Serviços</NuxtLink>
+        </div>
+      </div>
+  
+      <div>
+        <div class="container mx-auto">
+          <h1 class="text-lg font-bold">Servicos</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
+            magni molestias praesentium rem tenetur! Aliquid aperiam at dicta
+            error est eum iste magni minus nihil non quisquam sit, unde voluptate.
+          </p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Servicos",
+  };
+  </script>
+  
